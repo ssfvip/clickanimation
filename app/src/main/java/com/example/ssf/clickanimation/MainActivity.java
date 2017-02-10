6ac28273e1c877dcbdde31c53a398c7953fa160e
H-Destiny <2026652784@qq.com>
2017-02-10 10:21:34 +0800

    实现点击动

--- app/src/main/java/com/example/ssf/clickanimation/MainActivity.java
+++ app/src/main/java/com/example/ssf/clickanimation/MainActivity.java
@@ -2,12 +2,24 @@ package com.example.ssf.clickanimation;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MyImageView joke;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        joke = (MyImageView) findViewById(R.id.c_joke);
+        joke.setOnClickIntent(new MyImageView.OnViewClickListener() {
+            @Override
+            public void onViewClick(MyImageView view) {
+                Toast.makeText(MainActivity.this, "you click joke", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
